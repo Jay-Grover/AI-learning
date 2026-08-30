d06f5a4a2b82f6b9ce7df2404540398ca5788a6f
Jay-Grover <75678419+Jay-Grover@users.noreply.github.com>
2026-08-31 00:03:32 +0530

    added model-grader prompt-eval

--- prompt-eval.py
+++ prompt-eval.py
@@ -22,13 +22,16 @@ client = OpenAI(
 
 model = "anthropic/claude-opus-4.6"
 
+
 def add_user_message(messages, text):
     user_msg = {"role": "user", "content": text}
     messages.append(user_msg)
 
+
 def add_assitant_message(messages, text):
     assitant_msg = {"role": "assitant", "content": text}
     messages.append(assitant_msg)
+
 
 def chat(messages):
     message = client.chat.completions.create(
@@ -40,43 +43,86 @@ def chat(messages):
     return message.choices[0].message.content
 
 
-# step2: eval dataset
-import json
-def generate_dataset():
-    prompt = """
-Generate a evaluation dataset for a prompt evaluation. The dataset will be used to evaluate prompts
-that generate Python, JSON, or Regex specifically for AWS-related tasks. Generate an array of JSON objects,
-each representing task that requires Python, JSON, or a Regex to complete.
+# # step2: eval dataset (so first create dataset)
+# import json
+# def generate_dataset():
+#     prompt = """
+# Generate a evaluation dataset for a prompt evaluation. The dataset will be used to evaluate prompts
+# that generate Python, JSON, or Regex specifically for AWS-related tasks. Generate an array of JSON objects,
+# each representing task that requires Python, JSON, or a Regex to complete.
+#
+# Example output:
+# ```json
+# [
+#     {
+#         "task": "Description of task",
+#     },
+#     ...additional
+# ]
+# ```
+#
+# * Focus on tasks that can be solved by writing a single Python function, a single JSON object, or a regular expression.
+# * Focus on tasks that do not require writing much code
+#
+# Please generate 3 objects.
+# """
+#     messages=[]
+#     add_user_message(messages, prompt)
+#     add_assitant_message(messages, "```json")
+#     text = chat(messages)
+#     if "```json" in text:
+#         text = text.split("```json")[1].split("```")[0].strip()
+#     elif "```" in text:
+#         text = text.split("```")[1].split("```")[0].strip()
+#     print(text)
+#     return json.loads(text)
+#
+# dataset = generate_dataset()
+# with open("dataset.json", "w") as f:
+#     json.dump(dataset,f,indent=2)
+# # print(dataset)
 
-Example output:
-```json
-[
-    {
-        "task": "Description of task",
-    },
-    ...additional
-]
-```
+# Step 2: pass through claude
 
-* Focus on tasks that can be solved by writing a single Python function, a single JSON object, or a regular expression.
-* Focus on tasks that do not require writing much code
-
-Please generate 3 objects.
-"""
+def run_prompt(test_case):
+    # merges the prompt and test case and returns the result
+    prompt_v1 = f"""
+    please solve the following task:
+    {test_case["task"]}
+    """
     messages=[]
-    add_user_message(messages, prompt)
-    add_assitant_message(messages, "```json")
-    text = chat(messages)
-    if "```json" in text:
-        text = text.split("```json")[1].split("```")[0].strip()
-    elif "```" in text:
-        text = text.split("```")[1].split("```")[0].strip()
-    print(text)
-    return json.loads(text)
-
-dataset = generate_dataset()
-with open("dataset.json", "w") as f:
-    json.dump(dataset,f,indent=2)
-# print(dataset)
+    add_user_message(messages,prompt_v1)
+    output = chat(messages)
+    return output
+    # pass
 
 
+def run_test_case(test_case):
+    # calls the run_prompt,then grades the result
+    output = run_prompt(test_case)
+    # todo grade
+    score = 10
+    return {
+        "output": output,
+        "test_case": test_case,
+        "score": score
+    }
+    # pass
+
+def run_eval(dataset):
+    # loads the dataset and calls run_test_case for each case
+    results = []
+    for test_case in dataset:
+        result = run_test_case(test_case)
+        results.append(result)
+
+    return results
+    # pass
+
+import json
+with open("dataset.json", "r") as f:
+    dataset = json.load(f)
+
+results = run_eval(dataset)
+
+print(json.dumps(results, indent=2))
